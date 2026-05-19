64de45d6fdf166f2e5b1a0eb45f6026ca9aca298
Danzz <102394910+Abang07@users.noreply.github.com>
2026-05-19 23:51:23 +0700

    Add WebSocket dependency to youtube.js

--- api/downloader/youtube.js
+++ api/downloader/youtube.js
@@ -1,3 +1,4 @@
+const WebSocket = require('ws')
 const CONFIG = {
   headers: {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36',
